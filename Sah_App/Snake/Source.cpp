9aabd053588854e7b7564f9d7e17082fda5596e2
RealMeKaNiK <dautovic2000@gmail.com>
2021-05-28 00:40:42 +0200

    Snake

--- Sah_App/Snake/Source.cpp
+++ Sah_App/Snake/Source.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 using namespace std;
+using namespace sf;
+
 
 int main()
 {
-	cout << "SNAKEEE ";
+	sf::RenderWindow window(sf::VideoMode(800, 800), "Snake");
+	window.setFramerateLimit(60);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		window.display();
+	}
 	return 0;
 }
